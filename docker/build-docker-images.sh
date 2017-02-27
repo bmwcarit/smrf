1a3d420dce89ace2779a07e766e9a6219a2343e4
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-02-27 07:53:33 +0100

    [Docker] added initial DockerMake files
    Change-Id: I1ca08d65382eafbb827326ec4c2836dc2b66e138

---
+++ docker/build-docker-images.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e -x
+
+function usage
+{
+    echo "usage: build.sh --repository mydockerrepo.org --tag tag"
+}
+
+DOCKER_MAKE=$(which docker-make)
+if [ ! -x "$DOCKER_MAKE" ] ; then
+    echo "could not find docker-make in PATH"
+    echo "install it using 'pip install DockerMake'"
+    exit 1
+fi
+
+TAG=latest
+
+while [ "$1" != "" ]; do
+    echo "PARAM is: $1"
+    case $1 in
+        -r | --repository )     shift
+                                echo "REPO"
+                                REPOSITORY=${1%/}
+                                ;;
+        -t | --tag )            shift
+                                TAG="$1"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+declare -a DOCKER_IMAGES=(
+    "smrf-base"
+    "smrf-cpp"
+)
+
+if [ $REPOSITORY ]; then
+    REPOSITORY="--repository $REPOSITORY"
+else
+    REPOSITORY=
+fi
+
+for IMAGE in "${DOCKER_IMAGES[@]}"
+do
+    $DOCKER_MAKE --makefile "${IMAGE}.yml" ${IMAGE} ${REPOSITORY} --tag ${TAG}
+done
