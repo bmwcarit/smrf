68fe8e0f6257a4f650dbf0278a3732802e932aa6
Manuel Schiller <Manuel.Schiller@bmw-carit.de>
2017-02-27 07:46:07 +0100

    [Java] added java API for libsmrf
    Change-Id: Ib554fda03277a4f6504a9f6306a8368eaa52b828

---
+++ java/smrf-api/src/main/java/io/smrf/MessageSerializer.java
@@ -0,0 +1,37 @@
+package io.smrf;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+public interface MessageSerializer {
+    public MessageSerializer encryptFor(X509Certificate cert);
+    public byte[] serialize() throws SecurityException, EncodingException;
+    public MessageSerializer setBody(byte[] body);
+    public MessageSerializer setCompressed(boolean isCompressed);
+    public MessageSerializer setHeaders(Map<String, String> headers);
+    public MessageSerializer setRecipient(String recipient);
+    public MessageSerializer setSender(String sender);
+    public MessageSerializer setTtlMs(long ttlMs);
+    public MessageSerializer setTtlAbsolute(boolean ttlAbsolute);
+    public MessageSerializer signWith(X509Certificate cert, PrivateKey key);
+}
