5fc690ef0c6b205b1bec305142a5c942f6d6c4cd
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-04-19 10:48:44 +0200

    [Java] added MessagePrefix
    Change-Id: Ia2b0fa716ad803fcc6de20006471ac5e12cd40b7

---
+++ java/smrf/src/main/java/io/smrf/MessagePrefix.java
@@ -0,0 +1,95 @@
+package io.smrf;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class MessagePrefix {
+
+    /**
+     * offset of version field in bytes
+     */
+    private final static int VERSION_OFFSET = 0;
+
+    /**
+     * size of version field in bytes
+     */
+    private final static int VERSION_SIZE = 1;
+
+    /**
+     * offset of msgSize field in bytes
+     */
+    private final static int MSG_SIZE_OFFSET = VERSION_OFFSET + VERSION_SIZE;
+
+    /**
+     * size of msgSize field in bytes
+     */
+    private final static int MSG_SIZE_SIZE = 4;
+
+    /**
+     * offset of sigSize field in bytes
+     */
+    private final static int SIG_SIZE_OFFSET = MSG_SIZE_OFFSET + MSG_SIZE_SIZE;
+
+    /**
+     * size of msgSize field in bytes
+     */
+    private final static int SIG_SIZE_SIZE = 4;
+
+    /**
+     * size of message prefix in bytes
+     */
+    public final static int SIZE = SIG_SIZE_OFFSET + SIG_SIZE_SIZE;
+
+    /**
+     * current version of SMRF message format
+     */
+    public final static int VERSION = 1;
+
+    public byte version = VERSION;
+    public int msgSize = 0;
+    public int sigSize = 0;
+
+    public MessagePrefix(byte[] serializedMessage) throws EncodingException {
+
+        if (serializedMessage.length < SIZE) {
+            throw new EncodingException("message size too small");
+        }
+
+        final ByteBuffer prefixBuffer = ByteBuffer.wrap(serializedMessage, VERSION_OFFSET, SIZE)
+                .order(ByteOrder.LITTLE_ENDIAN);
+
+        this.version = prefixBuffer.get(VERSION_OFFSET);
+        this.msgSize = prefixBuffer.getInt(MSG_SIZE_OFFSET);
+        this.sigSize = prefixBuffer.getInt(SIG_SIZE_OFFSET);
+    }
+
+    public MessagePrefix() {
+    }
+
+    public void writeToPreallocatedBuffer(ByteBuffer serializedMessage) {
+        assert (serializedMessage.capacity() >= SIZE);
+
+        serializedMessage.put(version);
+        serializedMessage.putInt(msgSize);
+        serializedMessage.putInt(sigSize);
+    }
+}
---
+++ java/smrf/src/test/java/io/smrf/MessagePrefixTest.java
@@ -0,0 +1,67 @@
+package io.smrf;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.Parameters;
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
+public class MessagePrefixTest {
+
+    @Test(expected = EncodingException.class)
+    @Parameters
+    public void tooShortInputCausesException(int size) throws EncodingException {
+        byte[] bytes = new byte[size];
+        @SuppressWarnings("unused")
+        MessagePrefix messagePrefix = new MessagePrefix(bytes);
+    }
+
+    public Integer[] parametersForTooShortInputCausesException() {
+        Integer[] array = new Integer[MessagePrefix.SIZE];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    @Test
+    public void roundtrip() throws EncodingException {
+        MessagePrefix expectedMessagePrefix = new MessagePrefix();
+        expectedMessagePrefix.version = Byte.MAX_VALUE;
+        expectedMessagePrefix.msgSize = Integer.MAX_VALUE;
+        expectedMessagePrefix.sigSize = Integer.MAX_VALUE;
+
+        ByteBuffer buffer = ByteBuffer.allocate(MessagePrefix.SIZE).order(ByteOrder.LITTLE_ENDIAN);
+        expectedMessagePrefix.writeToPreallocatedBuffer(buffer);
+
+        MessagePrefix messagePrefix = new MessagePrefix(buffer.array());
+        assertEquals(expectedMessagePrefix.version, messagePrefix.version);
+        assertEquals(expectedMessagePrefix.msgSize, messagePrefix.msgSize);
+        assertEquals(expectedMessagePrefix.sigSize, messagePrefix.sigSize);
+    }
+}
