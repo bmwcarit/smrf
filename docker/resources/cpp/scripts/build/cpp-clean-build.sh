b15b5ba5cff25e8850eb3b74955e23c5f77ec586
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-05-04 13:05:16 +0200

    [C++] optionally allow installation in C++ build script
    Change-Id: Ie35b5b7d00bdcdd4146d0c3e0c3445575f915f91

--- docker/resources/cpp/scripts/build/cpp-clean-build.sh
+++ docker/resources/cpp/scripts/build/cpp-clean-build.sh
@@ -5,10 +5,11 @@ source /data/src/docker/resources/base/scripts/global.sh
 ENABLE_CLANG_FORMATTER='ON'
 BUILD_TESTS='ON'
 ADDITIONAL_CMAKE_ARGS=''
+INSTALL='OFF'
 
 function usage
 {
-    echo "usage: cpp-clean-build.sh [--jobs NUM_JOBS --enableclangformatter ON|OFF --buildtests ON|OFF --additionalcmakeargs <args>]"
+    echo "usage: cpp-clean-build.sh [--jobs NUM_JOBS --enableclangformatter ON|OFF --buildtests ON|OFF --additionalcmakeargs <args> --install ON|OFF]"
 }
 
 while [ "$1" != "" ]; do
@@ -25,13 +26,20 @@ while [ "$1" != "" ]; do
         --additionalcmakeargs )  shift
                                  ADDITIONAL_CMAKE_ARGS=$1
                                  ;;
+        --install )              shift
+                                 INSTALL=$1
+                                 ;;
         * )                      usage
                                  exit 1
     esac
     shift
 done
 
-log "CPP CLEAN BUILD: JOBS=$JOBS, ENABLE_CLANG_FORMATTER=$ENABLE_CLANG_FORMATTER, BUILD_TESTS=$BUILD_TESTS, ADDITIONAL_CMAKE_ARGS=$ADDITIONAL_CMAKE_ARGS"
+log "CPP CLEAN BUILD: JOBS=$JOBS, \
+     ENABLE_CLANG_FORMATTER=$ENABLE_CLANG_FORMATTER, \
+     BUILD_TESTS=$BUILD_TESTS, \
+     ADDITIONAL_CMAKE_ARGS=$ADDITIONAL_CMAKE_ARGS, \
+     INSTALL=$INSTALL"
 
 log "ENVIRONMENT"
 env
@@ -49,3 +57,8 @@ cmake -DENABLE_CLANG_FORMATTER=$ENABLE_CLANG_FORMATTER \
       $SOURCE_DIRECTORY
 
 make -j $JOBS
+
+if [ "$INSTALL" == "ON" ]
+then
+  make install
+fi
