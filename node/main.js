7c6344e7739787733cfc7b49101e92f98ba17358
Atanas Gegov <atanas.gegov@bmw-carit.de>
2017-05-17 14:14:28 +0200

    [node] Require binary part using the .node file
    Change-Id: I539ca5f8a35cf8a679724ac499514bb45e2f110c

--- node/main.js
+++ node/main.js
@@ -20,9 +20,9 @@
 
 var smrfNative;
 try {
-    smrfNative = require('smrf-native-cpp');
+    smrfNative = require('smrf-native-cpp.node');
 } catch (err) {
-    smrfNative = require('bindings')('smrf-native-cpp');
+    smrfNative = require('bindings')('smrf-native-cpp.node');
 }
 
 function deserialize(buffer)
