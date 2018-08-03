3cae7ea880fd153204bc70251aea3e1ef670be9f
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2018-08-03 05:54:48 +0000

    [JS] fix message with string ttl test
    smrf-js and smrf-native throw TypeErrors with
    different messages. Only the Error type matters here.
    
    Change-Id: Id9216e4bbe7e8e317e6c4ce906a0a963fde956ac

--- javascript/test/serialize.test.js
+++ javascript/test/serialize.test.js
@@ -68,8 +68,7 @@ for (var key in message) {
 test('message with ttl as string throws', t => {
     let corruptedMessage = clone(message);
     corruptedMessage.ttlMs = 'blubber';
-    const error = t.throws(() => {
+    t.throws(() => {
         smrf.serialize(corruptedMessage);
     }, TypeError);
-    t.is(error.message, 'value is not a number: ' + corruptedMessage.ttlMs);
 });
